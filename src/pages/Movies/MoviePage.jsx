996cbacf81fe870836aaf998c3ccb7951c481637
JunHyeok Yoon <jhy230912@gmail.com>
2025-04-24 03:08:55 +0900

    add sorting

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -9,9 +9,14 @@ import Spinner from "react-bootstrap/esm/Spinner";
 import { Alert } from "bootstrap";
 import "./MoviePage.style.css";
 import pageStore from "../../stores/pageStore";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
 
 const Moviepage = () => {
   const { page, updatePage } = pageStore();
+  const [sortPopularity, setSortPopularity] = useState(null);
+  const [sortGenre, setSortGenre] = useState(null);
   const [query, setQuery] = useSearchParams();
   const keyword = query.get("q");
 
@@ -19,6 +24,8 @@ const Moviepage = () => {
     keyword,
     page,
   });
+
+  const { data: genreData } = useMovieGenreQuery();
 
   if (isLoading) {
     return <Spinner animation="border" />;
@@ -32,12 +39,68 @@ const Moviepage = () => {
     updatePage(selected + 1);
   };
 
-  console.log(data);
+  const sortHandler = () => {
+    let sortMovieList = data?.results;
+
+    if (sortGenre !== null) {
+      sortMovieList = sortMovieList.filter((movie) =>
+        movie.genre_ids.includes(sortGenre)
+      );
+    }
+
+    if (sortPopularity !== null) {
+      switch (sortPopularity) {
+        case "popularity":
+          sortMovieList.sort((a, b) => b.vote_count - a.vote_count);
+          break;
+        case "unpopularity":
+          sortMovieList.sort((a, b) => a.vote_count - b.vote_count);
+          break;
+        default:
+          break;
+      }
+    }
+
+    // updatePage(1);
+    return sortMovieList;
+  };
 
   return (
     <Container>
       <Row>
-        <Col lg={4} xs={12}></Col>
+        <Col lg={4} xs={12} className="sort-box">
+          <DropdownButton id="dropdown-basic-button" title="인기 순">
+            <Dropdown.Item
+              onClick={() => {
+                setSortPopularity("popularity");
+                updatePage(1);
+              }}
+            >
+              많은 순
+            </Dropdown.Item>
+            <Dropdown.Item
+              onClick={() => {
+                setSortPopularity("unpopularity");
+                updatePage(1);
+              }}
+            >
+              적은 순
+            </Dropdown.Item>
+          </DropdownButton>
+          <DropdownButton id="dropdown-basic-button" title="장르 별">
+            {genreData?.map((genre, index) => (
+              <Dropdown.Item
+                key={index}
+                onClick={() => {
+                  setSortGenre(genre.id);
+                  updatePage(1);
+                }}
+              >
+                {genre.name}
+              </Dropdown.Item>
+            ))}
+          </DropdownButton>
+        </Col>
         {data?.results.length === 0 ? (
           <Col lg={8} xs={12} className="no-search">
             <p>검색 결과가 없습니다</p>
@@ -45,7 +108,7 @@ const Moviepage = () => {
         ) : (
           <Col lg={8} xs={12}>
             <Row>
-              {data?.results.map((movie, index) => (
+              {sortHandler().map((movie, index) => (
                 <Col key={index} lg={4} xs={6}>
                   <MovieCard movie={movie} />
                 </Col>
